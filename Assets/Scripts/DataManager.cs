24cd948048507d47a951d66e5cefa5463c23d027
AlienHunterD <dbiediger@gmail.com>
2017-11-12 22:48:35 -0600

    Initial Check-in

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+//using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public class DataManager
+{
+    private static DataManager _Instance;
+
+    int _dimX = 0, _dimY = 0, _dimZ;
+    float _voxelSize = 1.0f; // in mm
+
+    private Texture2D _emptyTexture;
+
+    public static DataManager Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = new DataManager();
+            }
+            return _Instance;
+        }
+    }
+
+    private DataManager()
+    {
+        _emptyTexture = new Texture2D(1, 1);
+        _emptyTexture.SetPixel(0, 0, Color.clear);
+        _emptyTexture.Apply();
+    }
+
+
+}
